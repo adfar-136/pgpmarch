098a9d2d38ee7bf7139bbb999d96d4e3cbe2572c
adfar-136 <adfarrasheed136@gmail.com>
2025-04-02 21:45:37 +0530

    02-04-2025

---
+++ JavaScript/functions.js
@@ -0,0 +1,9 @@
+function greet(){
+    console.log("adfar")
+}
+function square(x,y,z){
+    console.log(x*x+y+z)
+    
+}
+// var x = square(5,6,7)
+console.log(square(45,56,6))
---
+++ JavaScript/loops.js
@@ -0,0 +1,12 @@
+// for(let i =10 ;i>0;i--){
+//     console.log(i)
+// }
+// let i = 20
+// while(i>10){
+//   console.log(i);
+//   i--;
+// }
+
+do {
+    console.log("Adfar")
+} while(0)
